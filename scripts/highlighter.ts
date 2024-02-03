8e6db436a614ed0678d088cfab2c657ed15a27ec
Will Scullin <willscullin@google.com>
2024-02-03 07:36:58 -0800

    Bundle build script (#149)
    * Bundle build script
    
    * Copy required worker script
    
    * update malloy again
    
    ---------
    
    Co-authored-by: lloyd tabb <ltabb@google.com>

--- scripts/highlighter.ts
+++ scripts/highlighter.ts
@@ -62,6 +62,10 @@ const malloySQLTMGrammar = JSON.parse(
 
 const HIGHLIGHTER = shiki.getHighlighter({
   theme: "light-plus",
+  paths: {
+    themes: "node_modules/shiki/themes/", 
+    languages: "node_modules/shiki/languages/"
+  },
   langs: [
     "sql",
     "json",
