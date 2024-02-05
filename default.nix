4456d4edb818c779827f604792639189d3192dca
Will Scullin <willscullin@google.com>
2024-02-05 21:42:23 +0000

    Update nix

--- default.nix
+++ default.nix
@@ -1,1 +1,14 @@
-with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-18_x google-cloud-sdk git cacert openssh fakeroot]; }
+with import <nixpkgs> { }; stdenv.mkDerivation {
+  name = "malloy";
+  buildInputs = [
+    nodejs-18_x
+    google-cloud-sdk
+    git
+    cacert
+    openssh
+    fakeroot
+    libuuid
+  ];
+  APPEND_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [ pkgs.libuuid ]}";
+  shellHook = ''export LD_LIBRARY_PATH="$APPEND_LIBRARY_PATH:$LD_LIBRARY_PATH"'';
+}
